cf6af2bfd27e5d60d583ac7f5c797dae9e36cbbc
Vanitha20 <36623944+Vanitha20@users.noreply.github.com>
2018-02-20 10:47:47 +0530

    Create Hello.java

---
+++ BeginnerLevel/Hello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Hello{
+public static void main(String args[]){
+int n=0;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+for(int i=1;i<=n;i++)
+System.out.println("Hello");
+}
+}
