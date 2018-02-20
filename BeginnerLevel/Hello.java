d24a8dd7354c62e1c050d5e321e54598ae7c14d3
Vanitha20 <36623944+Vanitha20@users.noreply.github.com>
2018-02-20 19:17:35 +0530

    Update Hello.java

--- BeginnerLevel/Hello.java
+++ BeginnerLevel/Hello.java
@@ -1,10 +1,16 @@
 import java.util.*;
 class Hello{
-public static void main(String args[]){
+public static void main(String args[])throws Exception{
+try{
 int n=0;
 Scanner s=new Scanner(System.in);
 n=s.nextInt();
 for(int i=1;i<=n;i++)
 System.out.println("Hello");
 }
+}
+catch(Exception e){
+  System.out.println("invalid input");
+  }
+  }
 }
