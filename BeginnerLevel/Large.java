fdb6a761bba629818dcb6959892a457dcb44c6ab
Vanitha20 <36623944+Vanitha20@users.noreply.github.com>
2018-02-21 21:04:29 +0530

    Create Large.java

---
+++ BeginnerLevel/Large.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Large{
+public static void main(String args[])throws Exception{
+try{
+int a,b,c;
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+b=s.nextInt();
+c=s.nextInt();
+if(a>b&&a>c)
+System.out.println(a);
+else if(b>c&&b>a)
+System.out.println(b);
+else
+System.out.println(c);
+}
+catch(Exception e){
+System.out.println("enter input as number");
+}
+}
+}
