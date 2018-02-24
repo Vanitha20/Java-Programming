38f304cf892d2c2f68d4cd7e6e2928bd84a95c2e
Vanitha20 <36623944+Vanitha20@users.noreply.github.com>
2018-02-24 20:53:45 +0530

    Update Large.java

--- BeginnerLevel/Large.java
+++ BeginnerLevel/Large.java
@@ -7,9 +7,9 @@ Scanner s=new Scanner(System.in);
 a=s.nextInt();
 b=s.nextInt();
 c=s.nextInt();
-if(a>b&&a>c)
+if(a>=b&&a>=c)
 System.out.println(a);
-else if(b>c&&b>a)
+else if(b>=c&&b>=a)
 System.out.println(b);
 else
 System.out.println(c);
