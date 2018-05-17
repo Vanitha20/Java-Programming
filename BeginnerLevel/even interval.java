f6c852be79dedff72ebdbef1042d78ba91056c47
Vanitha20 <36623944+Vanitha20@users.noreply.github.com>
2018-05-17 18:44:16 +0530

    Create even interval.java

---
+++ BeginnerLevel/even interval.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class evenint{
+public static void main(String args[])throws Exception{
+try{
+Scanner s=new Scanner(System.in);
+System.out.println("enter two numbers");
+int n=s.nextInt();
+int m=s.nextInt();
+for(int i=n+1;i<=m;i++){
+if(i%2!=0)
+System.out.print(i+" ");
+}
+}
+catch(Exception e){
+System.out.println("invalid input");
+}
+}
+}
