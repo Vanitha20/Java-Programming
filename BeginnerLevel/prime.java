bf8d8be48fb67839c7d6d9be64ee37faa0529ac2
Vanitha20 <36623944+Vanitha20@users.noreply.github.com>
2018-05-17 19:23:20 +0530

    Create prime.java

---
+++ BeginnerLevel/prime.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Prime{
+public static void main(String args[])throws Exception{
+try{
+int c=0;
+Scanner s=new Scanner(System.in);
+System.out.println("enter a number");
+int n=s.nextInt();
+if(n<=1000){
+for(int i=1;i<=n;i++){
+if(n%i==0)
+c++;
+}
+if(c==2)
+System.out.println("Yes");
+else
+System.out.println("No");
+}
+else
+System.out.println("enter input within the range");
+}
+catch(Exception e){
+System.out.println("invaild input");
+}
+}
+}
