8e56ed0e7d3a4b8c773342fd2ede51750b2d7720
Vanitha20 <36623944+Vanitha20@users.noreply.github.com>
2018-06-15 18:43:29 +0530

    Create Countw.java

---
+++ BeginnerLevel/Countw.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Wordco{
+public static void main(String args[]){
+try{
+int c=1;
+Scanner s=new Scanner(System.in);
+String n=s.nextLine();
+if(n.length()<=1000){
+for(int i=0;i<n.length();i++)
+if(n.charAt(i)==' ')
+c+=1;
+System.out.println(c);
+}
+}
+catch(Exception e){
+System.out.println("Invalid input");
+}
+}
+}
