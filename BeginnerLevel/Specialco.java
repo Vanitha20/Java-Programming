c54a7c277db9177651130931695152a85805cb91
Vanitha20 <36623944+Vanitha20@users.noreply.github.com>
2018-06-15 20:46:03 +0530

    Create Specialco.java

---
+++ BeginnerLevel/Specialco.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Specco{
+public static void main(String args[]){
+try{
+int c=0;
+Scanner s=new Scanner(System.in);
+String n=s.nextLine();
+if(n.length()<=1000){
+for(int i=0;i<n.length();i++)
+if(!Character.isDigit(n.charAt(i))&&!Character.isLetter(n.charAt(i)))
+c+=1;
+System.out.println(c);
+}
+}
+catch(Exception e){
+System.out.println("Invalid input");
+}
+}
+}
+
