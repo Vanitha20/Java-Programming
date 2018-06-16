5da18d166d28d4d4a4be9601881111822f3cb752
Vanitha20 <36623944+Vanitha20@users.noreply.github.com>
2018-06-16 14:53:26 +0530

    Create Addeo.java

---
+++ BeginnerLevel/Addeo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Addeo{
+public static void main(String args[]){
+try{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int m=s.nextInt();
+int v=n+m;
+if(v%2==0)
+System.out.println("even");
+else
+System.out.println("odd");
+}
+catch(Exception e){
+System.out.println("Invalid input");
+}
+}
