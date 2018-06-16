67928442b0f2e76679096e9a96c39137cecec1d8
Vanitha20 <36623944+Vanitha20@users.noreply.github.com>
2018-06-16 11:47:44 +0530

    Rename Sum.java to Sumt.java

---
+++ BeginnerLevel/Sumt.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Sum{
+public static void main(String args[]){
+try{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int m=s.nextInt();
+System.out.println(n+m);
+}
+catch(Exception e){
+System.out.println("Invalid input");
+}
+}
+}
