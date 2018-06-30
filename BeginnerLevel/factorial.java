a8b1f401fba90c56a88a13922cd052bd6e4a5a53
Vanitha20 <36623944+Vanitha20@users.noreply.github.com>
2018-06-30 20:09:18 +0530

    Update factorial.java

--- BeginnerLevel/factorial.java
+++ BeginnerLevel/factorial.java
@@ -1,14 +1,14 @@
 import java.util.*;
 class Fact{
-public static void main(String args[])throws Exception{
+public static void main(String args[]){
 try{
 int f=1;
 Scanner s=new Scanner(System.in);
-System.out.println("enter a number");
 int n=s.nextInt();
-if(n>0){
-for(int i=1;i<=n;i++)
+if(n<=20){
+for(int i=1;i<=n;i++){
 f=f*i;
+}
 System.out.println(f);
 }
 else
