afaadc3c2a64c88aa050df6c5a48b02951bd9c85
Vanitha20 <36623944+Vanitha20@users.noreply.github.com>
2018-07-01 21:07:20 +0530

    Create FirstCap.java

---
+++ BeginnerLevel/FirstCap.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Firstc{
+public static void main(String args[]){
+String m="",p="";
+Scanner s=new Scanner(System.in);
+String n=s.nextLine();
+String a[]=n.split(" ");
+for(int j=0;j<a.length;j++){
+for(int i=0;i<a[j].length();i++){
+if(i==0){
+m=(String.valueOf(a[j].charAt(i))).toUpperCase();
+}
+else{
+p=p+(String.valueOf(a[j].charAt(i))).toLowerCase();
+}
+}
+System.out.print(m+p);
+p="";
+System.out.print(" ");
+}
+}
+}
