355a943999559fee5b399c8c1ea793962bf8fba8
Vanitha20 <36623944+Vanitha20@users.noreply.github.com>
2018-07-01 21:11:32 +0530

    Update FirstCap.java

--- BeginnerLevel/FirstCap.java
+++ BeginnerLevel/FirstCap.java
@@ -5,6 +5,7 @@ String m="",p="";
 Scanner s=new Scanner(System.in);
 String n=s.nextLine();
 String a[]=n.split(" ");
+ if(n.length()<1000000){
 for(int j=0;j<a.length;j++){
 for(int i=0;i<a[j].length();i++){
 if(i==0){
@@ -17,6 +18,7 @@ p=p+(String.valueOf(a[j].charAt(i))).toLowerCase();
 System.out.print(m+p);
 p="";
 System.out.print(" ");
+}
 }
 }
 }
